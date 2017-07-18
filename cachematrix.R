1d36d3fad38b28d652de6e5f9ddb9bb029b07522
DipankarBhowmik <Bhowmikd1984@gmail.com>
2017-07-18 23:27:05 +0530

    ProgrammingAssignment inprove
    Documentation

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,12 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function() inv
+  get <- function() x                                 #get the value of the matrix
+  setInverse <- function(inverse) inv <<- inverse     #set the value of the invertible matrix
+  getInverse <- function() inv                        #get the value of the invertible matrix
+  ##create list with methods for get/set of both original matrix 
+  ##and its inverse and return to list of parent environment. 
+  ##this allows use of $ operator to access each function from list
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -22,12 +25,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
+  if(!is.null(inv)){                                  #if inverse is not NULL
+    message("getting cached data")                    #Type message: Getting Cached Matrix
+    return(inv)                                       #return invertible matrix
   }
-  data <- x$get()
-  inv <- solve(data)
-  x$setInverse(inv)
-  inv      
+  data <- x$get()                                     #get the original matrix data
+  inv <- solve(data)                                  #use solve() to inverse the matrix
+  x$setInverse(inv)                                   #set the invertible matrix
+  inv                                                 #return a matrix that is the inverse of x
 }
